38e5a1d73a2d36d7193cdb068f3f9b30ca740e6d
Jackie Wong <wongzjackie@gmail.com>
2023-08-02 00:10:21 -1000

    Update tailwind.config.js
    - Configure extension from Tailwind's default theme.
    - Add custom colors
    - Add and configure fonts from Next.js self-hosted Google Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +10,41 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: colors.black,
+        white: colors.white,
+        gray: {
+          100: '#f4f4f5',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          400: '#a1a1aa',
+          500: '#71717a',
+          600: '#52525b',
+          700: '#3f3f46',
+          800: '#27272a',
+          900: '#18181b',
+        },
+        blue: {
+          200: '#bfdbfe',
+          300: '#8ED0FF',
+          500: '#44A8FF',
+          600: '#0284c7',
+          700: '#0369a1',
+          800: '#075985',
+          900: '#0c4a6e',
+        },
+        amber: {
+          100: '#fffbeb',
+          200: '#FFF5EC',
+          300: '#fef3c7',
+          400: '#fde68a',
+        },
+      },
+      fontFamily: {
+        'playfair-display': 'var(--font-playfair-display)',
+        'dm-sans': 'var(--font-dm-sans)',
       },
     },
   },
   plugins: [],
-}
+};
